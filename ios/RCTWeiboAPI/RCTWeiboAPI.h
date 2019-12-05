5a8486ff4d50bcbe0af6dd6ed0660d3428a436ce
JackYu <jackcool1989@outlook.com>
2019-12-05 10:06:59 +0800

    fix alltobid

--- ios/RCTWeiboAPI/RCTWeiboAPI.h
+++ ios/RCTWeiboAPI/RCTWeiboAPI.h
@@ -6,7 +6,13 @@
 //  Copyright © 2016 erica. All rights reserved.
 //
 
+#if __has_include(<React/RCTBridgeModule.h>)
+#import <React/RCTBridgeModule.h>
+#elif __has_include("RCTBridgeModule.h")
 #import "RCTBridgeModule.h"
+#elif __has_include("React/RCTBridgeModule.h")
+#import "React/RCTBridgeModule.h"
+#endif
 
 @interface RCTWeiboAPI : NSObject<RCTBridgeModule>
 
